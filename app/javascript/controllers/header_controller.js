479b92549f781545d89c0c7b9a0091a9e7029229
Aman Pardeshi <amanpardeshi@yahoo.in>
2022-10-07 13:10:37 +0530

    adding additional files

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,28 @@
+import { enter, leave, toggle } from 'el-transition';
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['openUserMenu'];
+  connect() {
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    const dropdownButton = document.getElementById('dropdown-button');
+    function openMenu() {
+      enter(dropdownMenu);
+    }
+
+    function closeMenu() {
+      leave(dropdownMenu);
+    }
+
+    function toggleDropdownMenu() {
+      toggle(dropdownMenu);
+    }
+
+    dropdownButton.addEventListener('click', toggleDropdownMenu);
+    document.addEventListener('click', (event) => {
+      if (!dropdownButton.contains(event.target)) {
+        toggleDropdownMenu();
+      }
+    });
+  }
+}
---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
